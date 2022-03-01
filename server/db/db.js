0b13fcf15f1acec7a63e5bc15319e6b9a1bbf0eb
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-03-01 10:59:36 -0500

    pkg.json changes

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
 const db = new Sequelize(
 	process.env.DATABASE_URL || "postgres://localhost:5432/tasks",
-	{ logging: false }
+	{ logging: true }
 );
 
 module.exports = db;
